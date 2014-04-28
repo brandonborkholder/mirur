feea072d82a8fbd2d0162a8eee8cc74e863ced1f
Brandon Borkholder <borkhobs@gmail.com>
2014-04-27 21:35:38 -0400

    added glimpse dependencies

---
+++ src/main/java/testplugin/views/GlimpseArrayView.java
@@ -0,0 +1,42 @@
+package testplugin.views;
+
+import javax.media.opengl.GLProfile;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+import com.jogamp.opengl.util.FPSAnimator;
+import com.metsci.glimpse.painter.base.GlimpsePainter;
+import com.metsci.glimpse.painter.info.FpsPainter;
+import com.metsci.glimpse.plot.Plot2D;
+import com.metsci.glimpse.swt.canvas.NewtSwtGlimpseCanvas;
+
+public class GlimpseArrayView extends ViewPart {
+    private NewtSwtGlimpseCanvas canvas;
+    private FPSAnimator animator;
+
+    @Override
+    public void createPartControl(Composite parent) {
+        canvas = new NewtSwtGlimpseCanvas(parent, GLProfile.getGL2GL3(), SWT.DOUBLE_BUFFERED);
+        animator = new FPSAnimator(canvas.getGLDrawable(), 20);
+        animator.start();
+
+        GlimpsePainter painter = new FpsPainter();
+        Plot2D plot = new Plot2D("none");
+        plot.addPainter(painter);
+
+        canvas.addLayout(plot);
+    }
+
+    @Override
+    public void setFocus() {
+        canvas.getCanvas().setFocus();
+    }
+
+    @Override
+    public void dispose() {
+        animator.stop();
+        canvas.dispose();
+    }
+}
