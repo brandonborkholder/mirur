9f221563eca23a642f9f44ce0c67547c60c8ab70
Brandon Borkholder <borkhobs@gmail.com>
2014-06-02 10:54:54 -0400

    added test project for debugging

---
+++ test-workspace/simple/src/simple/Main.java
@@ -0,0 +1,12 @@
+package simple;
+
+public class Main {
+    public static void main(String[] args) {
+    	double[] random = new double[500];
+    	for (int i = 0; i < random.length; i++) {
+    		random[i] = Math.random();
+    	}
+    	
+    	System.out.println("Debug here");
+    }
+}
