e6089e351a26d85e705b3b44381c3deb721852ee
Brandon Borkholder <borkhobs@gmail.com>
2016-09-24 20:33:56 -0400

    more variables to test

--- mirur-test/src/main/java/mirur/test/ManyVariablesTest.java
+++ mirur-test/src/main/java/mirur/test/ManyVariablesTest.java
@@ -4,8 +4,10 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Random;
 
 import mirur.core.Array1DImpl;
+import mirur.core.Array2DRectangular;
 import mirur.core.VariableObject;
 import mirur.plugins.MirurView;
 import mirur.plugins.bar1d.BarView;
@@ -16,7 +18,7 @@ public class ManyVariablesTest extends Harness {
     private Queue<Runnable> q;
 
     public ManyVariablesTest() {
-        q = join(createViews(), createArrays());
+        q = join(createViews(), createArrays1d(), createArrays2d());
 
         q.add(new Runnable() {
             public void run() {
@@ -27,12 +29,22 @@ public class ManyVariablesTest extends Harness {
         nextPushed();
     }
 
-    private Queue<Runnable> join(List<MirurView> views, List<Object> arrays) {
+    private Queue<Runnable> join(List<MirurView> views, List<Object> arrays1d, List<Object> arrays2d) {
         Queue<Runnable> q = new ArrayDeque<Runnable>();
 
         for (final MirurView view : views) {
-            for (Object array : arrays) {
+            for (Object array : arrays1d) {
                 final VariableObject obj = new Array1DImpl("tmp", array);
+                if (view.supportsData(obj)) {
+                    q.add(new Runnable() {
+                        public void run() {
+                            setView(view, obj);
+                        }
+                    });
+                }
+            }
+            for (Object array : arrays2d) {
+                final VariableObject obj = new Array2DRectangular("tmp", array);
                 if (view.supportsData(obj)) {
                     q.add(new Runnable() {
                         public void run() {
@@ -56,10 +68,50 @@ public class ManyVariablesTest extends Harness {
         return views;
     }
 
-    private List<Object> createArrays() {
+    private List<Object> createArrays1d() {
         List<Object> arrays = new ArrayList<Object>();
 
-        arrays.add(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        Random r = new Random(0);
+
+        {
+            int[] a = new int[300];
+            for (int i = 0; i < a.length; i++) {
+                a[i] = r.nextInt(100);
+            }
+            arrays.add(a);
+        }
+        {
+            float[] a = new float[1000];
+            for (int i = 0; i < a.length; i++) {
+                a[i] = r.nextFloat();
+            }
+            arrays.add(a);
+        }
+        {
+            double[] a = new double[1000];
+            for (int i = 0; i < a.length; i++) {
+                a[i] = 5 + r.nextDouble() * 1e-8;
+            }
+            arrays.add(a);
+        }
+
+        return arrays;
+    }
+
+    private List<Object> createArrays2d() {
+        List<Object> arrays = new ArrayList<Object>();
+
+        Random r = new Random(0);
+
+        {
+            float[][] a = new float[30][1000];
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a[i].length; j++) {
+                    a[i][j] = r.nextFloat();
+                }
+            }
+            arrays.add(a);
+        }
 
         return arrays;
     }
